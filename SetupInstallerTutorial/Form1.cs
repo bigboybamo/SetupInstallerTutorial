a2efd748e18e9303fb58300e7b5ed333bbf93ab0
Bamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2024-03-22 01:14:40 +0100

    Add project files.

---
+++ SetupInstallerTutorial/Form1.cs
@@ -0,0 +1,15 @@
+namespace SetupInstallerTutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Setup Installer Guide";
+        }
+    }
+}
